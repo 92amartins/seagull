e1d14a517b917999b18cdbcb0520c75bf97fd24e
Andrei <amartins13@outlook.com>
2014-07-03 14:40:29 +0100

    Added class to test Classification.java and Report.java

---
+++ Seagull/src/processing/ClassificationTest.java
@@ -0,0 +1,26 @@
+package processing;
+
+import processing.Classification.ClassifierType;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class ClassificationTest {
+	
+	public static void main(String[] args) {
+		try{
+			Instances data = DataSource.read("C:\\Program Files\\Weka-3-6\\data\\iris.arff");
+			data.setClassIndex(data.numAttributes()-1);
+			Classification cls = new Classification(ClassifierType.NAIVE_BAYES);
+			Report r = new Report(cls.performCrossValidation(data, 10));
+			
+			System.out.println(r.summaryString());
+			System.out.println("\n \n \n");
+			System.out.println(r.classificationDetails());
+			System.out.println("\n \n \n");
+			System.out.println(r.confusionMatrix());
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
