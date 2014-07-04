2e64e238c1f5fe9de9a912cccc6a8dd1a5083f3b
camillamaciel <camilla.leicam@yahoo.com>
2014-07-04 12:39:39 +0100

    Adding textArea to show the Classification results.

--- Seagull/src/ui/ClassificationPanel.java
+++ Seagull/src/ui/ClassificationPanel.java
@@ -1,10 +1,13 @@
 package ui;
 
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class ClassificationPanel extends JPanel{
 	
@@ -15,6 +18,7 @@ public class ClassificationPanel extends JPanel{
 	private JScrollPane paneOptions;
 	private JLabel lblReport;
 	private JScrollPane paneReport;
+	private JTextArea txtAreaReport;
 	private JButton btnSave;
 	private JButton btnChart;
 	
@@ -27,7 +31,8 @@ public class ClassificationPanel extends JPanel{
 		lblOptions = new JLabel("Options: ");
 		paneOptions = new JScrollPane();		
 		lblReport = new JLabel("Report:");
-		paneReport = new JScrollPane();
+		txtAreaReport = new JTextArea();
+		paneReport = new JScrollPane(txtAreaReport);
 		btnSave = new JButton("Save to file");
 		btnChart = new JButton("Generate chart");
 		
@@ -54,6 +59,8 @@ public class ClassificationPanel extends JPanel{
 		add(lblReport);
 		
 		paneReport.setBounds(200, 40, 560, 370);
+		txtAreaReport.setEditable(false);
+		txtAreaReport.setFont(new Font("Courier New", Font.PLAIN, 12));
 		add(paneReport);
 		
 		btnSave.setBounds(530, 420, 100, 25);
@@ -61,6 +68,70 @@ public class ClassificationPanel extends JPanel{
 		
 		btnChart.setBounds(640, 420, 120, 25);
 		add(btnChart);		
+	}
+
+	public JButton getBtnBrowse() {
+		return btnBrowse;
+	}
+
+	public void setBtnBrowse(JButton btnBrowse) {
+		this.btnBrowse = btnBrowse;
+	}
+
+	public JButton getBtnProcess() {
+		return btnProcess;
+	}
+
+	public void setBtnProcess(JButton btnProcess) {
+		this.btnProcess = btnProcess;
+	}
+
+	public JProgressBar getProgressBar() {
+		return progressBar;
+	}
+
+	public void setProgressBar(JProgressBar progressBar) {
+		this.progressBar = progressBar;
+	}
+
+	public JScrollPane getPaneOptions() {
+		return paneOptions;
+	}
+
+	public void setPaneOptions(JScrollPane paneOptions) {
+		this.paneOptions = paneOptions;
+	}
+
+	public JScrollPane getPaneReport() {
+		return paneReport;
+	}
+
+	public void setPaneReport(JScrollPane paneReport) {
+		this.paneReport = paneReport;
+	}
+
+	public JTextArea getTxtAreaReport() {
+		return txtAreaReport;
+	}
+
+	public void setTxtAreaReport(JTextArea txtAreaReport) {
+		this.txtAreaReport = txtAreaReport;
+	}
+
+	public JButton getBtnSave() {
+		return btnSave;
+	}
+
+	public void setBtnSave(JButton btnSave) {
+		this.btnSave = btnSave;
+	}
+
+	public JButton getBtnChart() {
+		return btnChart;
+	}
+
+	public void setBtnChart(JButton btnChart) {
+		this.btnChart = btnChart;
 	}
 
 }
