e70344c57c88322445cc6939cb4b567d5ba1a287
camillamaciel <camilla.leicam@yahoo.com>
2014-07-09 12:47:25 +0100

    Adding fields to Model.

--- Seagull/src/model/PreProcessingModel.java
+++ Seagull/src/model/PreProcessingModel.java
@@ -1,5 +1,63 @@
 package model;
 
-public class PreProcessingModel {
+import java.util.ArrayList;
 
+public class PreProcessingModel {
+	
+	private String path;	
+	private boolean tokenization;
+	private boolean stemming;
+	private boolean stopwords;
+	private boolean normalization;
+	//TODO private ??? bagOfWords;
+	private ArrayList<String> filesList;
+	
+	public String getPath() {
+		return path;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public boolean isTokenization() {
+		return tokenization;
+	}
+	
+	public void setTokenization(boolean tokenization) {
+		this.tokenization = tokenization;
+	}
+	
+	public boolean isStemming() {
+		return stemming;
+	}
+	
+	public void setStemming(boolean stemming) {
+		this.stemming = stemming;
+	}
+	
+	public boolean isStopwords() {
+		return stopwords;
+	}
+	
+	public void setStopwords(boolean stopwords) {
+		this.stopwords = stopwords;
+	}
+	
+	public boolean isNormalization() {
+		return normalization;
+	}
+	
+	public void setNormalization(boolean normalization) {
+		this.normalization = normalization;
+	}
+	
+	public ArrayList<String> getFilesList() {
+		return filesList;
+	}
+	
+	public void setFilesList(ArrayList<String> filesList) {
+		this.filesList = filesList;
+	}
+	
 }
