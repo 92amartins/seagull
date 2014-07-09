47a030208d49992db908f593f5b7f9e16181b768
camillamaciel <camilla.leicam@yahoo.com>
2014-07-09 12:47:07 +0100

    Indenting.

--- Seagull/src/input/Input.java
+++ Seagull/src/input/Input.java
@@ -9,16 +9,10 @@ import weka.core.*;
 
 public class Input {
 
-	
 	public Input(){
-		
-		
-		
+
 	}
-	
-	
-	
-	
+
 	public static void print_list(List<Cell> l){
 		String pick = new String();
 		int i = 0;
@@ -27,70 +21,55 @@ public class Input {
 			pick = (l.get(i)).getText();
 			i++;
 			System.out.println(pick);
-			
-			
 		}
-		
-		
-		return;
-		
-	}
-	
 
-	
-	
-	
+		return;
+	}
+
 	public static List<Cell> read_file (String path, String classe, String original_file) {
 		Stopwords checker = new Stopwords();
 		List<Cell> wBag = new ArrayList<Cell>();
 		Scanner sc2 = null;
 		Cell s;		
 		int counter =0;
-	    try {
-	        sc2 = new Scanner(new File(path));
-	    } catch (FileNotFoundException e) {
-	    	System.out.println("erro");
-	        e.printStackTrace();  
-	    }
-	    while (sc2.hasNextLine()) {
-	            Scanner s2 = new Scanner(sc2.nextLine());
-	        //boolean b;
-	        while (s2.hasNext()) {
-	        	s = new Cell();
-	            s.setText((s2.next().toLowerCase()).replaceAll("[^A-Za-z0-9]", ""));
-	            s.setClasse(classe);
-	            s.setOriginal_file(original_file);
-	            
-	            if((s.getText() != "") && !checker.is(s.getText())){
-	            	wBag.add(s);
-	            	
-	            }else{
-	           	counter++;
-	            }
+		try {
+			sc2 = new Scanner(new File(path));
+		} catch (FileNotFoundException e) {
+			System.out.println("erro");
+			e.printStackTrace();  
+		}
+		while (sc2.hasNextLine()) {
+			Scanner s2 = new Scanner(sc2.nextLine());
+			//boolean b;
+			while (s2.hasNext()) {
+				s = new Cell();
+				s.setText((s2.next().toLowerCase()).replaceAll("[^A-Za-z0-9]", ""));
+				s.setClasse(classe);
+				s.setOriginal_file(original_file);
 
-	        }
-	        s2.close();
-	    }
-	    
-	    sc2.close();
-	    
-	  
-	   //System.out.println("counter: " + counter);
-	    
+				if((s.getText() != "") && !checker.is(s.getText())){
+					wBag.add(s);
+
+				}else{
+					counter++;
+				}
+
+			}
+			s2.close();
+		}
+
+		sc2.close();
+
+
+		//System.out.println("counter: " + counter);
+
 		return wBag;
-		
+
 	}
 
-
-	
-	
-	
-	
-	
-	
-/*	
+	/*	
 	public static List<Cell> read_file (String path) {
-		
+
 		List<Cell> wBag = new ArrayList<Cell>();
 		Scanner sc2 = null;
 		Cell s;			//###################################################
@@ -106,39 +85,31 @@ public class Input {
 	        while (s2.hasNext()) {
 	        	s = new Cell();
 	            s.setText(s2.next().replaceAll("[^A-Za-z0-9]", ""));
-	            
+
 	            if(s.getText() != "") wBag.add(s);
 
 	        }
 	        s2.close();
 	    }
-	    
+
 	    sc2.close();
-	    
-	    
-	   
-	    
+
+
+
+
 		return wBag;
-		
+
 	}
-	
-	*/
-	
-	
-	
-	
-	
-	
-	
-	
+	 */
+
 	public List<List<List<Cell>>> read_subfolder(String path){
 		/*	reads the classes	*/
 		/*	Classe<arquivos<Bag of words>>	*/
 		List<List<List<Cell>>> bigBag = new ArrayList<>();
-		
+
 		List<Cell> bag;
 		List<List<Cell>> classe;
-		
+
 		int content_number;
 		String name_test;
 		String txt = new String("txt");
@@ -146,37 +117,34 @@ public class Input {
 		List<String> file_names = new ArrayList<String>();
 		String new_path = new String();
 		System.out.println("Lendo sub pastas:");
-		
-		
+
 		File f = new File(path);
 		List<String> content = new ArrayList<String>(Arrays.asList(f.list()));
-		
+
 		/*	an ArrayList of all the names of the content in the "path" are in content variable	*/
-		
-		
+
 		/*	Now the program will get only the sub folder names	*/
-		
-		
-		 content_number = content.size();
-		 
+
+		content_number = content.size();
+
 		for(int i=0; i<content_number;i++){
-			
+
 			name_test = content.get(i);
-			
+
 			if( (name_test.charAt(name_test.length() - 4) != '.')){
 				/*	add the sub folder to the list	*/
 				sub_folders.add(name_test);
 				continue;
 			}
-			  
+
 		}
-		
+
 		/*	at this point there is a list of the sub folders in the sub_folders variable	*/
-		
+
 		/*	access all folders	*/
-		
+
 		System.out.println("Obtendo arquivos nas sub pastas...");
-		
+
 		for(int i=0; i < sub_folders.size(); i++){
 			//System.out.println("#####################");
 			//System.out.println("Criando classe " + sub_folders.get(i));
@@ -184,41 +152,30 @@ public class Input {
 			new_path = path + '/' + sub_folders.get(i);
 			f = new File(new_path);
 			//System.out.println("Populando classe:");
-			
+
 			content = new ArrayList<String>(Arrays.asList(f.list()));
 			content_number = content.size();
 			for(int j=0; j< content_number; j++){
 				name_test = content.get(j);
 				/* if((name_test.substring(name_test.lastIndexOf('.') + 1)).equals(txt)){	 */
-					//System.out.println("Criando bag of words para :" + new_path + '/' + name_test);
-					bag = read_file(new_path + '/' + name_test, sub_folders.get(i), name_test);
-					//System.out.println("Adicionando   " + name_test + "   à classe.");
-					classe.add(bag);
+				//System.out.println("Criando bag of words para :" + new_path + '/' + name_test);
+				bag = read_file(new_path + '/' + name_test, sub_folders.get(i), name_test);
+				//System.out.println("Adicionando   " + name_test + "   ï¿½ classe.");
+				classe.add(bag);
 				/* } */
 			}
 			if(content_number != 0){
-			//System.out.println("Adicionando classe à big bag of words.");
-			bigBag.add(classe);
+				//System.out.println("Adicionando classe ï¿½ big bag of words.");
+				bigBag.add(classe);
 			}else{
 				//System.out.println("Classe vazia, desconsiderar.");
 			}
 		}
-		
-		
-		
-		
+
 		System.out.println("Isso eh tudo pessoal.");
 		System.out.println("Big Bag of Words (BBW) mounted");
 		System.out.println("################");
 		return bigBag;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
