3236940c09f525e83f0d0c91f28c19feecaa0b8d
camillamaciel <camilla.leicam@yahoo.com>
2014-07-10 13:49:33 +0100

    Initial commit of the class to deal with the errors and show to the user.

---
+++ Seagull/src/error/ExceptionsHandler.java
@@ -0,0 +1,28 @@
+package error;
+
+import javax.swing.JOptionPane;
+
+import control.FrameListener;
+
+public class ExceptionsHandler {
+	
+	public static void showUploadProcessFileDialog() {
+		String msg = "Please, upload a file to be processed, either using the Pre-processing tab or the browse button.";
+		JOptionPane.showMessageDialog(FrameListener.getMainFrame(), msg, "Upload process file", JOptionPane.WARNING_MESSAGE);
+	}
+	
+	public static void showUnsupportedFileDialog() {
+		String msg = "The file choosen is not supported, please try again.";
+		JOptionPane.showMessageDialog(FrameListener.getMainFrame(), msg, "Unsupported file", JOptionPane.WARNING_MESSAGE);
+	}
+	
+	public static void showSelectClassifierDialog() {
+		String msg = "Please, select at least one Classifier to process.";
+		JOptionPane.showMessageDialog(FrameListener.getMainFrame(), msg, "Select classifier", JOptionPane.WARNING_MESSAGE);
+	}
+	
+	public static void showInputEMParameterDialog() {
+		String msg = "Please, input the value parameter for the evaluation method choosen.";
+		JOptionPane.showMessageDialog(FrameListener.getMainFrame(), msg, "Input evaluation method parameter", JOptionPane.WARNING_MESSAGE);
+	}
+}
