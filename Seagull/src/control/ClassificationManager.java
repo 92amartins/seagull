cd93154793e10111f33b69d0718b063e82f8b4c9
camillamaciel <camilla.leicam@yahoo.com>
2014-07-10 13:57:30 +0100

    Checking which evaluationMethod should be used. classificationModel.getInstance() instead of new ClassificationModel(). Getting the instances from classificationModel.getInstances().

--- Seagull/src/control/ClassificationManager.java
+++ Seagull/src/control/ClassificationManager.java
@@ -5,23 +5,38 @@ import processing.Classification;
 import processing.Report;
 import weka.classifiers.Evaluation;
 import weka.core.Instances;
-import weka.core.converters.ConverterUtils.DataSource;
 
 public class ClassificationManager {
 	
 	private ClassificationModel classificationModel;
 	
-	public String classify(String path) {
+	public ClassificationManager() {
+		classificationModel = ClassificationModel.getInstance();
+	}
+	
+	public String classify() {
 		String report = "";
 		try{
-			Instances data = DataSource.read(path);
+			Instances data = classificationModel.getInstances();
 			data.setClassIndex(data.numAttributes()-1);
 			Classification cls = new Classification(classificationModel.getClassifierTypes());
-			
+			switch (classificationModel.getEvaluationMethod()) {
+				case CROSS_VALIDATION:
+					cls.performCrossValidation(data, classificationModel.getAdditionalParamEM());
+					break;
+				case PERCENTAGE_SPLIT:
+					cls.performTestSetEvaluation(data, classificationModel.getAdditionalParamEM());
+					break;
+				case LOOCV:
+					cls.performLOOCV(data);
+					break;
+			}
+						
 			Evaluation[] evals = cls.getEvaluations();
 			Report[] r = new Report[evals.length];
 			StringBuffer sb = new StringBuffer();
 			
+			//TODO show which classifier and evaluation method are being shown
 			for(int i = 0; i < evals.length;i++){
 				r[i] = new Report(evals[i]);
 				
@@ -30,7 +45,7 @@ public class ClassificationManager {
 				sb.append(r[i].classificationDetails());
 				sb.append("\n \n \n");
 				sb.append(r[i].confusionMatrix());
-				sb.append("==========================================================");
+				sb.append("==============================================================================================\n");
 			}
 			
 			report = sb.toString();
@@ -39,5 +54,13 @@ public class ClassificationManager {
 			e.printStackTrace();
 		}
 		return report;
+	}
+
+	public ClassificationModel getClassificationModel() {
+		return classificationModel;
+	}
+
+	public void setClassificationModel(ClassificationModel classificationModel) {
+		this.classificationModel = classificationModel;
 	}
 }
