e172d974e397e27827278e5481a90cde121a44f2
camillamaciel <camilla.leicam@yahoo.com>
2014-07-14 13:27:10 +0100

    Adding preProcessigModel field.

--- Seagull/src/control/PreProcessingManager.java
+++ Seagull/src/control/PreProcessingManager.java
@@ -6,9 +6,16 @@ public class PreProcessingManager {
 
 	private PreProcessingModel preProcessingModel;
 	
-	
-	
-	
-	
+	public PreProcessingManager() {
+		preProcessingModel = PreProcessingModel.getInstance();
+	}
+
+	public PreProcessingModel getPreProcessingModel() {
+		return preProcessingModel;
+	}
+
+	public void setPreProcessingModel(PreProcessingModel preProcessingModel) {
+		this.preProcessingModel = preProcessingModel;
+	}
 	
 }
