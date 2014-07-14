e58ff316bd70c785dd9eafaae61fc635c04ef988
camillamaciel <camilla.leicam@yahoo.com>
2014-07-14 14:12:00 +0100

    Adding inputValidFoldersStructure dialog.

--- Seagull/src/error/ExceptionsHandler.java
+++ Seagull/src/error/ExceptionsHandler.java
@@ -6,6 +6,11 @@ import control.FrameListener;
 
 public class ExceptionsHandler {
 	
+	public static void showSelectFoldersStructureDialog() {
+		String msg = "Please, select a valid folders structure.";
+		JOptionPane.showMessageDialog(FrameListener.getMainFrame(), msg, "Select valid folders structure", JOptionPane.WARNING_MESSAGE);
+	}
+	
 	public static void showUploadProcessFileDialog() {
 		String msg = "Please, upload a file to be processed, either using the Pre-processing tab or the browse button.";
 		JOptionPane.showMessageDialog(FrameListener.getMainFrame(), msg, "Upload process file", JOptionPane.WARNING_MESSAGE);
