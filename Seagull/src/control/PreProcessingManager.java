d240da4c38499897a4e2d5658d3aa545b34f441e
Andrei <amartins13@outlook.com>
2014-07-14 18:18:46 +0100

    Added startPreProcessing()

--- Seagull/src/control/PreProcessingManager.java
+++ Seagull/src/control/PreProcessingManager.java
@@ -1,6 +1,12 @@
 package control;
 
+import input.InstancesGenerator;
+import model.ClassificationModel;
 import model.PreProcessingModel;
+import preprocessing.BigBagReducer;
+import preprocessing.StemmingMaster;
+import preprocessing.StopwordRemover;
+import preprocessing.Weighter;
 
 public class PreProcessingManager {
 
@@ -16,6 +22,37 @@ public class PreProcessingManager {
 
 	public void setPreProcessingModel(PreProcessingModel preProcessingModel) {
 		this.preProcessingModel = preProcessingModel;
+	}
+	
+	public void startPreProcessing(){
+		InstancesGenerator ig = new InstancesGenerator();
+		BigBagReducer bbr = new BigBagReducer();
+		
+		// Removing repeated tokens
+		preProcessingModel.setBigBag(bbr.reduceBag(preProcessingModel.getBigBag()));
+		
+		
+		// STEMMING
+		if(preProcessingModel.isStemming()){
+			StemmingMaster sm = new StemmingMaster();
+			sm.StemmBag(preProcessingModel.getBigBag());
+		}
+		
+		// STOPWORDS
+		if(preProcessingModel.isStopwords()){
+			StopwordRemover swr = new StopwordRemover();
+			swr.removeAllStopwords(preProcessingModel.getBigBag());
+		}
+		
+		// NORMALIZATION
+		if(preProcessingModel.isNormalization()){
+			Weighter w = new Weighter();
+			preProcessingModel.setBigBag(w.weightTFIDF(preProcessingModel.getBigBag()));
+		}
+		
+		ClassificationModel cm = ClassificationModel.getInstance();
+		
+		cm.setInstances(ig.generate(preProcessingModel.getBigBag()));
 	}
 	
 }
